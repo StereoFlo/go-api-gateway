1b312c939345c8a25b1b4a60f3b3907b43c7d02d
StereoFlo <stereoflo@users.github.com>
2022-12-07 17:31:21 +0300

    improves on reverse proxy

--- http/proxy_handler.go
+++ http/proxy_handler.go
@@ -1,22 +1,47 @@
 package http
 
 import (
+	"errors"
 	"github.com/gin-gonic/gin"
 	"go_gw/infrastructure"
+	"go_gw/infrastructure/jwt-token"
 	"log"
 	"net/http"
 )
 
 func HandleProxy(ctx *gin.Context) {
+	responder := infrastructure.NewResponder()
+	err := checkToken(ctx)
+	if err != nil {
+		ctx.AbortWithStatusJSON(http.StatusUnauthorized, responder.Fail(err))
+		return
+	}
+
+	proxy(ctx, responder)
+}
+
+func proxy(ctx *gin.Context, responder *infrastructure.Responder) {
 	proxy := infrastructure.NewProxy(ctx)
-	err := make(chan error)
-	go proxy.ReverseProxy(err)
-	res := <-err
+	errCh := make(chan error)
+	go proxy.ReverseProxy(errCh)
+	res := <-errCh
 	if res != nil {
-		responder := infrastructure.NewResponder()
 		log.Println(res)
 		ctx.AbortWithStatusJSON(http.StatusNotFound, responder.Fail(res.Error()))
 		return
 	}
 	return
+}
+
+func checkToken(ctx *gin.Context) error {
+	token := ctx.Request.Header.Get("X-ACCOUNT-TOKEN")
+	if token == "" {
+		return errors.New("unauthorized")
+	}
+	jwt := jwt_token.NewToken()
+	_, err := jwt.Validate(token)
+	if err != nil {
+		return errors.New(err.Error())
+	}
+	return nil
 }
